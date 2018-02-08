3f900a868743ca54cf4dbcac7cf3d333f35932d5
David Axelrod <daxaxelrod@gmail.com>
2018-02-08 13:13:44 -0500

    notes through 2-8-2018

--- Fixed_Income/2-6-18.py
+++ Fixed_Income/2-6-18.py
@@ -1,4 +1,13 @@
 2-6-18
 Fixed income
 
+Homework due Thursday
+
 Linear interpolation
+    Taking the linear relationship between two know rates to guess the ytm
+
+Spent most of class going over a worksheet
+
+Implied future rates
+    IFR a,b-1  = {((1 + z @ b)**b) / ((1 + z @ a)**a)} ** 1/(b-a)
+    
---
+++ Fixed_Income/2-8-18.py
@@ -0,0 +1,23 @@
+Fixed Income
+2-8-18
+
+Goes over the hw
+Discount margin is the percentage you add to the discount rate (index + spread) that matches the price based upon the required rate of return investors require
+    The higher the Discount margin, the lower the price ends up being because cashflows are discounted more
+
+Spread: ytm minus closest maturity government rate
+G-Spread: YTM minus maturity matches government rate
+I-Spread: YTM minus maturity matched swap rate (generally fixed for variable interest rate swaps)
+Z-Spread: spread over current index
+
+IOS: Lets use an example
+    Start with a straight bond with price p
+    Then look at a bond with a call option
+    the price of the bond with the call option is <= the first bond
+    Spread call >= spread of normal bond
+
+    #callable bond
+    OIS = Spread of the call - call option value = Spread
+
+    #puttable bond
+    OAS = Spread of put + put option value = Spread
