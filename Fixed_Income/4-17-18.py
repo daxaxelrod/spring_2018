93a5d452f4de28f070d465c54de49e92fd151318
David Axelrod <daxaxelrod@gmail.com>
2018-04-17 11:36:18 -0400

    notes through 4-17-18

---
+++ Fixed_Income/4-17-18.py
@@ -0,0 +1,28 @@
+4-17-18
+
+Debt Payoff = Long RF security + Short put
+            = -Value of rf + Value of the put
+
+Determinants of credit risk
+    Value of the put = f(...)
+
+The payoff from a debt contract is equal to rf debt + a short put on the firm
+
+Determinants of credit risk
+    Volitilty of assets goes up, so does value of the put
+    Fv of debt goes up, the firm is borrowing more so the leverage presents a higher risk to the firm
+        So the value of the put goes up
+    asset value goes down, value of put goes up
+        If the stock price goes down the value of the put goes up
+
+Distance to Default model -- p(d)
+    Assumption: Firm defaults if Market value of asssets < Fv Debt
+    
+    Just the area of the z-score that the face value of the debt is away from the expected value of assets
+    Determinants of distance to default
+        FV debt
+        Std Dev - Volitility of assets
+        Asset Value
+
+Chapter 7
+    Asset backed Securities
