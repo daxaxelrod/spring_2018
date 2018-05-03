90577c35d90967b2126e242c4440175b80815b0f
David Axelrod <daxaxelrod@gmail.com>
2018-05-03 10:37:01 -0400

    final exam review

---
+++ Fixed_Income/5-3-18_FINAL_REVIEW.py
@@ -0,0 +1,128 @@
+Final exam review
+
+40-45 questions
+exam is going to be a reflection of what we did in class
+
+Went over the defining elements
+    features, bond clauses
+Issuance and funding
+    Size of the market
+    what types of markets exist
+    Types of bonds depending on their issuance location
+        Samuri bonds, panda bonds
+
+chapter 3 start quantitative
+Valuaion
+    Valuing bond from the yeild curve
+        Using the yeild curve and add a spread to reflect riskyness
+    also basic present value stuff
+
+Risk and return
+    Buy and hold vs selling before maturity
+    what happens to your portfolio if interest rates go up vs go down
+    How to compute anualized return
+    Constant price to yeild maturity
+        How to think about capital appreciation
+    Duration and convexity
+        Def going to be a duration a question
+        Portfolio duration
+        Litterally compute duration
+
+Arbitrage free valueation framework
+    The fucking tree
+    Shifting the tree up or down by a given riskiness level
+    Use the tree to value bond
+    Use it for callable, puttable, floaters, capped flaoters
+    Converatble bonds
+    Question on constructing tree
+    Questino on given tree and ask to compute the price
+
+Swaps
+    Interest rate swaps
+    dereive the valueation framework for interest rate models from scratch
+    know how they work
+    know how to value them
+        at time of start the fixed rate the price of the contract is the price the fixed party needs to pay
+        The value at the time 0 is 0
+        Depending on how i rates move, one party gets the advantage over another
+    CDS
+        HEdging default risk
+        Payout structre, how it works
+        Synthetically create a cds contract
+            Construct a portfolio of short the reference asset (the underlying bond) and then go long the risk free rate 
+            CDS spread is == spread on the underlying
+    
+Fundamentals of credit risk
+    new generation credit risk models
+    Derived from murtin in 1974
+    constructing the payoff of a bond using the options pricing model
+    Credit risk is the price of a put option with certain charasteristics
+        Strike at the face value of debt
+        Volitility of assets
+    drivers of credit risk are the same for a put option as they are for the actual bond
+    
+Liability driven investments
+    application of duration
+        pension fund. Facing a i rate problem from the liability duration is different from asset duration
+        How much of a specific duration bond should i buy to match duration (check the guys pwpt)
+
+Design of asset backed securities
+    SPV
+    Payment schedule
+        Of a mortage backed security
+    Traunches
+        How each traunch reduces credit risk vs reinvestment risk
+
+Projects
+    9 presentaions
+    Think of the main message (not authors names)
+    executive summaries important
+    Call features
+        holding all else constsant call should increase price of bond but the reality isnt true
+    financial distress
+        Reasons for bankruptcy vs restructing
+        Hodlout problems
+        Trust indenture act of 1939
+    Yeild curve
+        Alt ways to interperate
+        Shape of the yeild curve tells you x. what does it signal and why
+        Implied forward rates ties into this well
+    Investor paid credit rating
+        Investor vs issuer paid rating agencies
+        how investor paid agencies influence issuer
+            reputational concerns
+            makes s and ps rating more informative
+            conflict of interest
+    fixed income job mkt
+        no questions on this one
+        RIP
+    CDS and credit risk
+        Tail wags the dog
+        about how a contract that is designed to protect investors against default risk
+        Ends up increasing default risk of underlying entitie
+            Happens because ()()()()()() he didnt say in class so gotta fill in from execitive summary
+    Cash holdings
+        think of that one graph
+        u shaped relationship
+        triple a bonds have a lot of cash for a good reason
+        Junk bonds also hold cash too to pay off short term creditors but there is a lot of long term risks there
+        Influence of cash metrics on credit risk
+    Bond liquidity
+        Hey thats me
+        How to measure liquidity 
+            Bid ask spread and lot measure
+            Longer term bonds are less liquid
+            Riskier bonds are less liquid than safe bonds
+    Troubled debt restructing
+        Similar to the 2nd one
+        goes into the details of exchange offer and how its done
+        negotiation frictions and holdout problem 
+    
+        
+
+
+
+
+
+
+
